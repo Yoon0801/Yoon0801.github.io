d00d9d70fcf245152549464588743d3e37636b7c
Rein.Tsai <Rein.Tsai@hgiga.com>
2023-05-07 11:19:47 +0800

    # 完成 skill 區域 更新 qualification 區域

--- index.js
+++ index.js
@@ -22,3 +22,33 @@ $('#navClose').click(function () {
 $('.navLink').click(function () {
     $('#navMenu').removeClass('showMenu');
 });
+
+// skills part js
+$('.skillsHeader').on('click', function() {
+    console.log('123');
+    var itemClass = $(this).parent().attr('class');
+
+    $('.skillsContent').removeClass('skillsOpen').addClass('skillsClose');
+    if (itemClass === 'skillsContent skillsClose') {
+        $(this).parent().removeClass('skillsClose').addClass('skillsOpen');
+    }
+});
+
+// $('.skillsHeader').click(function () {
+//     var content = $(this).next('.skillsContent');
+
+//     if (content.hasClass('skillsClose')) {
+//         console.log('123');
+//         $('.skillsList').slideUp();
+//         $('.skillsContent').removeClass('skillsOpen');
+//         $('.skillsContent').addClass('skillsClose');
+//         content.slideDown();
+//         content.removeClass('skillsClose');
+//         content.addClass('skillsContent skillsOpen');
+//     } else {
+//         console.log('456');
+//         $('.skillsList').slideUp();
+//         content.removeClass('skillsOpen');
+//         content.addClass('skillsContent skillsClose');
+//     }
+// });
